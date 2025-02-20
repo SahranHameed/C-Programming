72b60622f1bd698537505dbb2de4f7cc353b0406
SahranHameed <sahranhmd2000@gmail.com>
2025-02-21 00:58:38 +0300

    Add files via upload

---
+++ Functions in - C/Predifined Library Function.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h> // (Predifined/Lib) Function = Mathematics All Function Diclare Here..
+int main ()
+
+{
+    int  x ;
+
+    x = pow (10,3); // (Power Of) Function Already Declaere So ... Only Call ... pow
+
+    printf("%d", x);
+}
---
+++ Functions in - C/Sample User Define Function.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void call ();
+
+int main ()
+{
+    call();
+
+    call();
+
+}
+
+
+void call ()
+{
+    printf("I am Called ");
+}
+
+
+
+
+
+
+
+
