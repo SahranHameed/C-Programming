7cf393fee2637497b6b7e9692e49d8c0614890f1
SahranHameed <sahranhmd2000@gmail.com>
2025-02-21 01:06:00 +0300

    Add files via upload

---
+++ Returns Statement/01. Without Argument  Without Returns.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+//void call ();
+
+
+
+void call ()
+{
+    int a = 5;
+    a = a + 3;
+
+    printf(" a = %d  ",a);
+}
+
+
+void main ()
+{
+    call();
+
+}
+
+
+
+///////////////////////////////////////////// ( scanf )
+#include<stdio.h>
+void call ();
+
+//void main ()
+//{
+//    call();
+
+//}
+
+
+//void call ()
+//{
+    int a , b , c;
+    //printf("Enter Your a Value : ");
+    //scanf("%d",&a);
+
+    //printf("Enter Your b Value : ");
+    //scanf("%d", &b);
+
+//    c = a + b;
+
+//    printf(" The Value Of c Is : %d  ",c);
+//}
+
+
+
+
+
+
+
+
+
---
+++ Returns Statement/02. With Argument  Without Returns.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+void add (int,int);
+
+void main()
+
+{
+   int a = 5, b = 6;
+   add (a , b);
+
+}
+
+void add (int x, int y)
+
+{
+    int z ;
+    z = x + y;
+    printf ("%d", z);
+}
+
+
+////////////////////////////////////// ( scanf  )
+#include<stdio.h>
+void add (int,int);
+
+//void main ()
+//{
+    int a,b;
+//    printf("Enter a Number: ");
+//    scanf ("%d",&a);
+//    printf("Enter b Number: ");
+//    scanf ("%d",&b);
+
+//    add (a,b);
+
+//}
+
+//void add ( int x, int y)
+
+//{
+
+    int z;
+//    z = x + y ;
+//    printf(" The Value Of Z Is : %d", z);
+
+//}
