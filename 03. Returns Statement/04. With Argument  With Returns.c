c0c8c2645133fdac057772e511b388935b8abf3c
SahranHameed <sahranhmd2000@gmail.com>
2025-02-21 09:09:48 +0300

    Add files via upload

---
+++ 03. Returns Statement/04. With Argument  With Returns.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int add (int,int);
+
+void main ()
+{
+    int a = 7, b = 8, c ;
+    c = add (a,b);
+    printf("%d", c);
+}
+
+int add (int x, int y)
+{
+    int z;
+    z = x + y ;
+    return z ;
+}
+
+
+///////////////////////////( scanf )
+#include<stdio.h>
+int add (int,int);
+
+//void main ()
+//{
+    int a, b, c ;
+//    printf(" Enter The a Value ");
+//    scanf("%d", &a);
+//    printf(" \nEnter The b Value ");
+//    scanf("%d", &b);
+
+//    c = add (a,b);
+//    printf("%d", c);
+
+//}
+
+//int add (int x, int y)
+//{
+    int z;
+//    z = x + y ;
+//    return z ;
+//}
