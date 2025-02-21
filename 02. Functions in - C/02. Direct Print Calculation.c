2754393efe69bc98f514e0c8b1d778149d8c801d
SahranHameed <sahranhmd2000@gmail.com>
2025-02-21 09:07:03 +0300

    Add files via upload

---
+++ 02. Functions in - C/02. Direct Print Calculation.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<math.h> // (Predifined/Lib) Function = Mathematics All Function Diclare Here..
+int main ()
+
+{
+
+    printf("%f", pow(10,2)); // (Power Of) Print Direct Calculation....  Function Already Declaere So ... Only Call ... pow
+
+}
