958991eac2655a390ee12517a0aaf3f994971802
SahranHameed <sahranhmd2000@gmail.com>
2025-02-21 08:59:32 +0300

    Add files via upload

---
+++ 01. Mathematics in - C/04. User Input  M. Variables.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+
+{
+    int a, b;
+    float c, d;
+
+    printf(" Enter A, B, C Numbers : ");
+    scanf ("%d %d %f ", &a, &b, &c);
+
+    d = a * b + c ;
+
+    printf(" The Value Of d Is : %f", d);
+}
